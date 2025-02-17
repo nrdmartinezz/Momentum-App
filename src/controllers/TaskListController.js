3738acd983367369685a66cb7b4a13556aa04ebc
Nate Martinez <nrd.martinezz@gmail.com>
2025-02-17 12:23:46 -0800

    Deploy To Firebase

--- src/controllers/TaskListController.js
+++ src/controllers/TaskListController.js
@@ -1,9 +1,33 @@
+
+
 class TaskListController {
     constructor(tasks) {
       this.tasks = tasks;
     }
-  
+
+    
+  createTask(task) {
+    this.tasks.push(task);
+  }
+
+  readTask(taskId) {
+    return this.tasks.find(task => task.id === taskId);
+  }
+
+  updateTask(taskId, updatedTask) {
+    const taskIndex = this.tasks.findIndex(task => task.id === taskId);
+    if (taskIndex !== -1) {
+      this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...updatedTask };
+    }
+  }
+
+  deleteTask(taskId) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+ 
     groupTasksByDate() {
+     
       const groupedTasks = this.tasks.reduce((groups, task) => {
         const date = new Date(task.dueDate).toDateString();
         if (!groups[date]) {
